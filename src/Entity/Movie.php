5695532e4c7a4e78bc56d576b5b59869b12b4a3b
Mathieu Trouvé <mathieu.trouve@live.fr>
2023-11-09 18:43:14 +0100

    Movie Entity

---
+++ src/Entity/Movie.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MovieRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: MovieRepository::class)]
+class Movie
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Title = null;
+
+    #[ORM\Column(length: 10, options:['default' => ''])]
+    private ?string $imdbID = '';
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->Title;
+    }
+
+    public function setTitle(string $Title): static
+    {
+        $this->Title = $Title;
+
+        return $this;
+    }
+
+    public function getImdbID(): ?string
+    {
+        return $this->imdbID;
+    }
+
+    public function setImdbID(string $imdbID): static
+    {
+        $this->imdbID = $imdbID;
+
+        return $this;
+    }
+}
