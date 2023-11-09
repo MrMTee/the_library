8fd191303ee417b2f42471fb2330fcfb765cde70
Mathieu Trouvé <mathieu.trouve@live.fr>
2023-11-09 18:45:34 +0100

    DB importer

---
+++ src/Api/Model/DatabaseImporter.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Api\Model;
+
+use App\Entity\Movie as MovieEntity;
+use App\Repository\MovieRepository;
+use Doctrine\ORM\EntityManagerInterface;
+
+class DatabaseImporter
+{
+
+    public function __construct(
+        private readonly EntityManagerInterface $entityManager,
+        private readonly MovieRepository $movieRepository
+    ) {
+    }
+
+    public function import(Movie $movie, bool $flush = false): MovieEntity
+    {
+
+        $entity = (new MovieEntity())
+            ->setTitle($movie->Title)
+            ->setImdbID($movie->ImdbID);
+
+        $this->entityManager->persist($entity);
+        $this->entityManager->flush();
+
+        return $entity;
+    }
+}
