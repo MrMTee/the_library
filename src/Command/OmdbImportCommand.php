ff1a4f9d95f49be596bdec6055ddddb9691c74da
Mathieu Trouvé <mathieu.trouve@live.fr>
2023-11-10 16:51:22 +0100

    Update command

--- src/Command/OmdbImportCommand.php
+++ src/Command/OmdbImportCommand.php
@@ -6,6 +6,9 @@ use App\Api\Client\ApiClientInterface;
 use App\Api\Model\DatabaseImporter;
 use App\Api\Model\Movie;
 use App\Api\Omdb;
+use App\Repository\MovieRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Exception;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
@@ -22,7 +25,9 @@ class OmdbImportCommand extends Command
 {
     public function __construct(
         private ApiClientInterface $omdbApiClient,
-        private DatabaseImporter $importer
+        private DatabaseImporter $importer,
+        private EntityManagerInterface $entityManager,
+        private MovieRepository $movieRepository
     ) {
         parent::__construct();
     }
@@ -38,27 +43,38 @@ class OmdbImportCommand extends Command
     {
         $io = new SymfonyStyle($input, $output);
         $search = $input->getArgument('search');
+        $isDryRun = $input->getOption('dry-run');
 
         if ($search) {
-            $io->note(sprintf('You passed : %s', $search));
+            $io->note(sprintf('Looking up OMDB for the search term : %s', $search));
         }
 
-        /**
-        if ($input->getOption('option1')) {
-           // ...
-        }
+        $imdbID = $io->choice('Which of these films do you want to import ?', $this->omdbApiClient->search($search));
         
-         */
-        $choice = $io->choice(
-            'Which of these result do you want to import ?',
-            $this->omdbApiClient->search($search)
+        // Update
+        if($this->movieRepository->findOneBy(['imdbID' => $imdbID])){
+            $update = $io->confirm('This film already exists, would you like to update it ?');
+            if(false === $update) return Command::INVALID;
+        }
+
+        //  Fetching movie
+        $movie = $this->omdbApiClient->getById($imdbID);
+        if(false === $isDryRun){
+        }
+
+        if (false === $isDryRun){
+            isset($update) ? $this->importer->update($movie, $imdbID) : $this->importer->import($movie);            
+            $io->success('Success');
+            $io->info(' >>>>>> The following was saved to database <<<<<<< ');
+        } else {
+            $io->info('Abort');
+            $io->info(' >>>>>> The following would have been imported : <<<<<<< ');
+        }
+
+        $io->createTable(
+            array_keys(get_object_vars($movie)),
+            [get_object_vars($movie)]
         );
-
-        $this->importer->import($this->omdbApiClient->getById($choice));
-    
-
-        $io->success('Success');
-
 
         return Command::SUCCESS;
     }
