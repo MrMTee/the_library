0d36c946aee21f459ad1758b73619469245c27e4
Mathieu Trouvé <mathieu.trouve@live.fr>
2023-11-16 14:23:02 +0100

    Person Entity

---
+++ src/Controller/Admin/PersonCrudController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Person;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class PersonCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Person::class;
+    }
+
+}
