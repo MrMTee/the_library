9c293e4e44e63d8ae89da2a8bea090da5ff4c43b
Mathieu Trouvé <mathieu.trouve@live.fr>
2023-11-16 15:26:01 +0100

    Comments Entity

---
+++ src/Entity/Comment.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CommentRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CommentRepository::class)]
+class Comment
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $Text = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getText(): ?string
+    {
+        return $this->Text;
+    }
+
+    public function setText(string $Text): static
+    {
+        $this->Text = $Text;
+
+        return $this;
+    }
+}
